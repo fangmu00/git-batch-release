c3caf0d4187b68e497e83cb4ba608e6e4abd9af7
Sun Guo <wb-bysunguo@alibaba-inc.com>
2017-06-30 17:56:13 +0800

    'new'

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # git-batch-release
 批量发布git
+
+## 安装
+```
+npm install
+npm link
+```
+
+## 使用方式
+1. 配置project.json
+```
+{
+    "react-demo": { // 项目名称
+        "proPath": "../react-demo", // 项目相对路径
+        "version": "daily/1.0.0" // 分支版本号
+    },
+    "demo1": {
+        "proPath": "../demo1",
+        "version": "daily/1.11.0" 
+    }
+    ....
+}
+```
+2. 命令行执行
+```
+// 提交所有项目
+app p -a 
+// 提交指定项目
+app p -i '项目名称按,分割'
+// 发布所有项目
+app pb -a 
+// 发指定项目
+app pb -i '项目名称按,分割'
+```
+
+## 优化点
+* 并发执行命令
+* 错误处理
+* 支持自定义指令
+
---
+++ src/index.js
@@ -0,0 +1,92 @@
+#! /usr/bin/env node
+
+const program = require('commander');
+const chalk = require('chalk');
+const child_process = require('child_process'); // js执行命令行
+const fs = require('fs');
+const path = require('path');
+
+const { resolve } = path;
+let file = null;
+
+try {
+    file = fs.readFileSync(resolve(__dirname, '../project.json')); // 获取项目配置文件
+} catch (error) {
+    console.log(`\n\n  ${chalk.red(error)}`);
+}
+if (!file) { return false }
+const fileJson = JSON.parse(file);
+
+const execSh = (commands, name) => {
+  commands.some(function(item) {
+    console.log((`\n${chalk.cyan('Executing:')} ${item}`));
+    let output;
+    try {
+      output = child_process.execSync(item, { cwd: fileJson[name].proPath });
+
+    } catch(err) {
+      console.log(`${chalk.red(err.stdout)}`);
+    }
+    output && process.stdout.write(output);
+
+    if  (output === undefined) {
+        return true;
+    }
+  });
+}
+
+const pushItem = (type, item) => {
+    console.log(`\n${chalk.yellow('Start runing: ')}${item}`);
+    let command = [
+        'git fetch origin',
+        'git merge origin/master',
+        'git add . --all',
+        'git commit -m \'build\'',
+        `git push origin ${fileJson[item].version}`,
+    ]
+    if (type === 'publish') {
+        const versionNumber = fileJson[item].version.split('/')[1];
+        console.log(versionNumber);
+        command = [
+            'git fetch origin',
+            'git merge origin/master',
+            `git tag publish/${versionNumber}`,
+            `git push origin -u publish/${versionNumber}`,
+        ]
+    }
+    let file = null;
+    try {
+        file = fs.readFileSync(resolve(__dirname, `../${fileJson[item].proPath}/.git/refs/heads/${fileJson[item].version}`));
+    } catch(err) {
+        console.log(`\n${chalk.red('Error: The git file is non-existent!')}`);
+    }
+    file && execSh(command, item);
+}
+
+const push = () =>{
+    program
+    .command('push')
+    .alias('p')
+    .description('提交代码')
+    .option('-a, --all','提交所有', ()=> { Object.keys(fileJson).forEach((item) => { pushItem('push',item); })})
+    .option('-i, --items [proName]','提交指定内容', (v)=> { const arr = v.split(','); arr.forEach((item) => { pushItem('push',item); })})
+    .action(option => {
+        console.log((`\n${chalk.green('End')}`));
+    });
+}
+
+
+const publish = () =>{
+    program
+    .command('publish')
+    .alias('pb')
+    .description('发布代码')
+    .option('-a, --all','发布所有', ()=> { Object.keys(fileJson).forEach((item) => { pushItem('publish',item); })})
+    .option('-i, --items [proName]','发布指定内容', (v)=> { const arr = v.split(','); arr.forEach((item) => { pushItem('publish',item); })})
+    .action(option => {
+        console.log((`\n${chalk.green('End')}`));
+    });
+}
+push();
+publish();
+program.parse(process.argv);
